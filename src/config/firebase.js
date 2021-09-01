e8dd863175213d1ba71de7869a8c6d5a87c9d22c
NikolaGolubovic <nikolagolubovic985@gmail.com>
2021-09-01 09:10:36 +0200

    forgoten env fixed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyBcAqLBcEMBEQ46ekuMWMHIfU5ZWzpTrJY",
-  authDomain: "auth-production-d23b4.firebaseapp.com",
-  databaseURL: "https://auth-production-d23b4.firebaseio.com",
-  projectId: "auth-production-d23b4",
-  storageBucket: "auth-production-d23b4.appspot.com",
-  messagingSenderId: "567473840342",
-  appId: "1:567473840342:web:c2fa9094d2d8de1688e977",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 });
 
 export const auth = app.auth();
